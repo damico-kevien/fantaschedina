97c8c59a42d6788fc567e1fa28f7d4bb8f4b4969
Kevien Damico <kevien.damico@fuoricitta.it>
2026-03-09 19:26:44 +0100

    Scaffolding

---
+++ src/main/java/com/fantacalcio/fantaschedina/FantaschedinaApplication.java
@@ -0,0 +1,13 @@
+package com.fantacalcio.fantaschedina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantaschedinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantaschedinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fantacalcio/fantaschedina/FantaschedinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantacalcio.fantaschedina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantaschedinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
